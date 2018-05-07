c033a7c5ecdc57d8f85b170127a1a2f2d86f5528
livia <liviamelq@gmail.com>
2018-05-07 16:13:53 +1000

    Change ajax request to add to basket function

---
+++ src/tests/add-to-basket.test.js
@@ -0,0 +1,9 @@
+const addToBasket = require("../components/add-to-basket.js");
+
+test("addToBasket function exists", () => {
+  expect(addToBasket).toBeDefined();
+});
+
+// test("String reverses", () => {
+//   expect(reverseString("hello")).toEqual("olleh");
+// });
